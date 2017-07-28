e8d9671fa70f21bfacdd53c5788d7f36f4c8a45d
vagrant <vagrant@homestead>
2017-07-28 07:23:23 +0000

    crash page

--- resources/views/errors/generic.blade.php
+++ resources/views/errors/generic.blade.php
@@ -22,7 +22,7 @@
             <div class="error-content">
                 <h3><i class="fa fa-warning text-red"></i> Oops! Something went wrong.</h3>
                 <p>
-                Please report this issue by clicking <a href="mailto:techsupport@hubwire.com">here</a> and we will work on fixing it right away (Please remember to include details about your session and how the error occured).
+                Please report this issue by clicking <a href="mailto:{{ env('SYSTEM_SUPPORT_EMAIL') }}">here</a> and we will work on fixing it right away (Please remember to include details about your session and how the error occured).
                 <br><br>
                 Meanwhile, you may <a href="{{route('data.index')}}">return to the main page</a>.
                 </p>
