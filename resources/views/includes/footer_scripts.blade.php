a5f5d55db8ff5343086c22d8380553070d440f1a
junezzao <jun@prog.com.my>
2017-08-28 02:45:43 +0000

    added js and css version

--- resources/views/includes/footer_scripts.blade.php
+++ resources/views/includes/footer_scripts.blade.php
@@ -1,5 +1,5 @@
 <script src="http://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" type="text/javascript"></script>
 
-<script src="{{ asset('js/app.min.js',env('HTTPS',false)) }}"></script>
+<script src="{{ asset('js/app.min.js',env('HTTPS',false)) }}?version={{ env('APP_VERSION') }}"></script>
 @yield('footer_scripts')
